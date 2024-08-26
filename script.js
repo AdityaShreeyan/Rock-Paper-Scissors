eca5bae0d36b66a27a877708511f30a796c232a1
AdityaShreeyan <shreeyanaditya@gmail.com>
2024-08-26 21:07:42 +0530

    Created a function that randomly selects between rock, paper & scissors.

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Hello World!");
+function getComputerChoice(){
+    choice = Math.floor(Math.random() * 3);
+    if (choice == 0){
+        return ("Rock");
+    }
+    else if (choice == 1){
+        return ("Paper");
+    }
+    else {
+        return ("Scissors");
+    }
+}
+console.log(getComputerChoice());
